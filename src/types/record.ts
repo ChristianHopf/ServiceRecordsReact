62f079c7c5f0af937cef873ff62470e7cf5dc890
ChristianHopf <cehopf@gmail.com>
2025-01-12 13:48:59 -0500

    Record interface

---
+++ src/types/record.ts
@@ -0,0 +1,13 @@
+export interface Record {
+  id: string;
+  owner: string;
+  date: string;
+  make: string;
+  model: string;
+  year: number;
+  vin: string;
+  license: string;
+  mileage: number;
+  service: string;
+  charge: number;
+}
